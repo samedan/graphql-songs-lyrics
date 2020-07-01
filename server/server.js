959ead0531d3c36a4b5d30b3137a9d206cc9756b
Daniel Popescu <same.dan@gmail.com>
2020-07-01 15:04:01 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Replace with your mongoLab URI
 const MONGO_URI =
-  'mongodb+srv://samedan78:Fuckyahoo667@cluster0-ciglp.mongodb.net/<dbname>?retryWrites=true&w=majority';
+  '';
 
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
